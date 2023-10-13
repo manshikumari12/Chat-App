587ed13dd2b324f6b3120439f2505c1225f72dd3
Manshi Kumari <119393324+manshikumari12@users.noreply.github.com>
2023-10-13 14:47:07 +0530

    chat app

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
-const express = require('express')
-const app = express()
-const {Server }=require('socket.io')
+const express = require("express");
+const app = express();
+const { Server } = require("socket.io");
 
-const http =require("http")
-const { Socket } = require('dgram')
 
-const httpServer=http.createServer(app)
-httpServer.listen(1111,()=>{
-    console.log("server is running at the port")
-})
+
+const http = require("http");
+const httpServer = http.createServer(app);
+httpServer.listen(8000,()=>{
+    console.log("server is running at port 8000")
+});
+
+
+
 
 const io = new Server(httpServer);
 
-
-
-
+const users = {};
+var count = 0;
 io.on("connection", (socket) => {
-    socket.on("new-user-joined", (name) => {
-      count+=1;
-      users[socket.id] = name;
-      socket.broadcast.emit("user-joined", name);
-      io.emit("user-online", count);
-    });
-  
-    socket.on("send", (message) => {
-      socket.broadcast.emit("receive", {
-        message: message,
-        name: users[socket.id],
-      });
-    });
-  
-  
-    socket.on("disconnect", (message) => {
-      socket.broadcast.emit("leave", users[socket.id])
-      delete users[socket.id];
-      count--;
-      if(count < 0) count = 0;
-      io.emit("user-online", count);
+  socket.on("new-user-joined", (name) => {
+    count+=1;
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+    io.emit("user-online", count);
+  });
+
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: users[socket.id],
     });
   });
+
+
+
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("leave", users[socket.id])
+    delete users[socket.id];
+    count--;
+    if(count < 0) count = 0;
+    io.emit("user-online", count);
+  });
+});
